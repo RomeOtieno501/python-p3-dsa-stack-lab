651be0a7f3bb032d158f1eb4a024374160865932
Rome Ojuro Otieno <otienorome2@gmail.com>
2024-12-30 10:13:41 +0300

    final commit

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,39 @@
 class Stack:
-
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=None, limit=100):
+        if items is None:
+            items = []
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            raise OverflowError("Stack is full")
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            raise IndexError("Pop from empty stack")
 
     def peek(self):
-        pass
-    
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            raise IndexError("Peek from empty stack")
+
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit
 
     def search(self, target):
-        pass
+        try:
+            return len(self.items) - 1 - self.items[::-1].index(target)
+        except ValueError:
+            return -1
